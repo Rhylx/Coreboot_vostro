7359eb26bf56905d9dbff9d4416a7dcfeb55f03a
Rhylx <redbull11570@gmail.com>
2019-12-16 23:39:31 +0100

    real new version

---
+++ build/bootblock/drivers/spi/flashconsole.d
@@ -0,0 +1,14 @@
+build/bootblock/drivers/spi/flashconsole.o: \
+ src/drivers/spi/flashconsole.c src/include/kconfig.h build/config.h \
+ src/include/rules.h src/commonlib/include/commonlib/compiler.h \
+ src/commonlib/include/commonlib/region.h src/include/sys/types.h \
+ src/include/sys/../types.h src/include/stdbool.h src/include/stdint.h \
+ src/include/stddef.h src/commonlib/include/commonlib/helpers.h \
+ src/commonlib/include/commonlib/compiler.h \
+ src/commonlib/include/commonlib/mem_pool.h src/include/boot_device.h \
+ src/include/fmap.h src/commonlib/include/commonlib/fmap_serialized.h \
+ src/include/console/console.h src/arch/x86/include/arch/cpu.h \
+ src/include/types.h src/include/console/post_codes.h \
+ src/commonlib/include/commonlib/loglevel.h \
+ src/include/console/vtxprintf.h src/include/console/flash.h \
+ src/include/stdlib.h
---
+++ build/postcar/drivers/spi/boot_device_rw_nommap.d
@@ -0,0 +1,11 @@
+build/postcar/drivers/spi/boot_device_rw_nommap.o: \
+ src/drivers/spi/boot_device_rw_nommap.c src/include/kconfig.h \
+ build/config.h src/include/rules.h \
+ src/commonlib/include/commonlib/compiler.h src/include/boot_device.h \
+ src/commonlib/include/commonlib/region.h src/include/sys/types.h \
+ src/include/sys/../types.h src/include/stdbool.h src/include/stdint.h \
+ src/include/stddef.h src/commonlib/include/commonlib/helpers.h \
+ src/commonlib/include/commonlib/compiler.h \
+ src/commonlib/include/commonlib/mem_pool.h src/include/spi_flash.h \
+ src/include/spi-generic.h src/include/boot/coreboot_tables.h \
+ src/commonlib/include/commonlib/coreboot_tables.h
